a6c46b4d1046c4c037b935cdb2377cdcad2897d1
Sewotr <sewotr@coursera.com>
2022-07-14 15:08:54 +0200

    New Version.

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 (function () {
 'use strict';
 
-var shoppingList = [
-  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-];
-
 angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+.controller('ShoppingListAddController', ShoppingListAddController)
+.controller('ShoppingListShowController', ShoppingListShowController)
+.service('ShoppingListService', ShoppingListService);
 
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList = shoppingList;
+ShoppingListAddController.$inject = ['ShoppingListService'];
+function ShoppingListAddController(ShoppingListService) {
+  var itemAdder = this;
+
+  itemAdder.itemName = "";
+  itemAdder.itemQuantity = "";
+
+  itemAdder.addItem = function () {
+    ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+  }
+}
+
+
+ShoppingListShowController.$inject = ['ShoppingListService'];
+function ShoppingListShowController(ShoppingListService) {
+  var showList = this;
+
+  showList.items = ShoppingListService.getItems();
+}
+
+
+function ShoppingListService() {
+  var service = this;
+
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(item);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
 }
 
 })();
