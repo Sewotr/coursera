97b585294f66ec589975fd5994783bebd22e2cdd
Sewotr <sewotr@coursera.com>
2022-07-15 13:24:30 +0200

    course assignment 2 progress

--- assignments/assignment2/assignment2-starter-code/app.js
+++ assignments/assignment2/assignment2-starter-code/app.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 
-angular.module('ControllerAsApp', [])
+angular.module('ShoppingListApp', [])
 .controller('ShoppingListController1', ShoppingListController1)
 .controller('ShoppingListController2', ShoppingListController2)
 .factory('ShoppingListFactory', ShoppingListFactory);
@@ -19,13 +19,9 @@ function ShoppingListController1(ShoppingListFactory) {
   list1.itemName = "";
   list1.itemQuantity = "";
 
-  list1.addItem = function () {
-    shoppingList.addItem(list1.itemName, list1.itemQuantity);
-  }
 
-  list1.removeItem = function (itemIndex) {
-    shoppingList.removeItem(itemIndex);
-  };
+
+
 }
 
 
